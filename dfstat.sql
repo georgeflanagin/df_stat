a2f5be4b657a14aded0e70d3058e8cf3e33d0100
ae9qg <ae9qg@spydur.cluster>
2024-03-19 15:33:49 -0400

    final commit before Skyler starts

---
+++ dfstat.sql
@@ -0,0 +1,4 @@
+CREATE TABLE df_stat(
+                            datetime default current_timestamp,
+                            directory varchar(10),
+                            memavail varchar(5));
