04fec13247d50c78fe80337605939403887174b7
George Flanagin <me@georgeflanagin.com>
2024-04-03 14:56:36 -0400

    Added skeleton analysis file.

---
+++ dfanalysis.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import typing
+from   typing import *
+
+min_py = (3, 8)
+
+###
+# Standard imports, starting with os and sys
+###
+import os
+import sys
+if sys.version_info < min_py:
+    print(f"This program requires Python {min_py[0]}.{min_py[1]}, or higher.")
+    sys.exit(os.EX_SOFTWARE)
+
+###
+# Other standard distro imports
+###
+import argparse
+import contextlib
+import getpass
+
+###
+# From hpclib
+###
+import linuxutils
+from   urdecorators import show_exceptions_and_frames as trap
+
+###
+# Credits
+###
+__author__ = 'George Flanagin'
+__copyright__ = 'Copyright 2022'
+__credits__ = None
+__version__ = 0.1
+__maintainer__ = 'George Flanagin'
+__email__ = ['gflanagin@richmond.edu', 'me@georgeflanagin.com']
+__status__ = 'in progress'
+__license__ = 'MIT'
+
+mynetid = getpass.getuser()
+
+
+@trap
+def handler(signum:int, stack:object=None) -> None:
+    """
+    Map SIGHUP and SIGUSR1 to a restart/reload, and 
+    SIGUSR2 and the other common signals to an orderly
+    shutdown. 
+    """
+    if signum in [ signal.SIGHUP, signal.SIGUSR1 ]: 
+        dfanalysis_main()
+
+    elif signum in [ signal.SIGUSR2, signal.SIGQUIT, signal.SIGINT ]:
+        logger.info(f'Closing up from signal {signum}')
+        fileutils.fclose_all()
+        sys.exit(os.EX_OK)
+
+    else:
+        return
+
+
+@trap
+def run_analysis() -> None
+    return
+
+
+
+@trap
+def dfanalysis_main(myargs:argparse.Namespace=None) -> int:
+
+    while True:
+        run_analysis()
+        time.sleep(3600)
+
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(prog="dfanalysis", 
+        description="What dfanalysis does, dfanalysis does best.")
+
+    parser.add_argument('-i', '--input', type=str, default="",
+        help="Input file name.")
+    parser.add_argument('-o', '--output', type=str, default="",
+        help="Output file name")
+    parser.add_argument('--nice', type=int, choices=range(0, 20), default=0,
+        help="Niceness may affect execution time.")
+    parser.add_argument('-v', '--verbose', action='store_true',
+        help="Be chatty about what is taking place")
+
+
+    myargs = parser.parse_args()
+    myargs.verbose and linuxutils.dump_cmdline(myargs)
+    if myargs.nice: os.nice(myargs.nice)
+
+    try:
+        outfile = sys.stdout if not myargs.output else open(myargs.output, 'w')
+        with contextlib.redirect_stdout(outfile):
+            sys.exit(globals()[f"{os.path.basename(__file__)[:-3]}_main"](myargs))
+
+    except Exception as e:
+        print(f"Escaped or re-raised exception: {e}")
+
+
